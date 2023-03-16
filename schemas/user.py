127bc1e0e004db542cc92e158eda23c74ab775ab
Edwin Hernandez <edwin.ejhnsn@gmail.com>
2023-03-15 23:33:37 -0500

    agregando users/items y haciendo arreglos con tiempo de expiracion

--- schemas/user.py
+++ schemas/user.py
@@ -35,3 +35,10 @@ class User(BaseModel):
                 "phone_number": "3213943876"
             }
         }
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+class TokenData(BaseModel):
+    username:str | None = None
--- service/token.py
+++ service/token.py
@@ -1,4 +1,5 @@
-from fastapi import HTTPException, Depends, status
+from fastapi import HTTPException, Depends, status, FastAPI
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from sqlalchemy.orm import session
 from datetime import datetime, timedelta
 import os
@@ -7,7 +8,8 @@ import os
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 
-from schemas.user import User as UserSchema
+import schemas.user as UserSchema
+
 from service.user import UserService as UserService
 from models.user import Users as UserModel
 
@@ -17,7 +19,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES"))
 
 
 
-class UserInDB(UserSchema):
+class UserInDB(UserSchema.User):
     hashed_password: str
 
 class user_token():
@@ -32,16 +34,27 @@ class user_token():
         return user
 
     async def get_current_user(token = Depends(UserService.oauth2_scheme),  db: session = Depends(UserService.get_db)):
-        user = user_token.decode_token(token, db)
-        if not user:
-            raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid authentication credentials",
-                headers={"WWW-Authenticate": "Bearer"},
-            )
+        credentials_exception = HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Could not validate credentials",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+        try:
+            payload=jwt.decode(token,SECRET_KEY,algorithms=[ALGORITHM])
+            username:str =payload.get("sub")
+            if username is None:
+                raise credentials_exception
+            token_data = UserSchema.TokenData(username=username)
+        except JWTError:
+        # Manejar la excepción aquí
+            raise credentials_exception
+        user = user_token.get_user(db,username=token_data.username)
+        if user is None:
+            raise credentials_exception
         return user
+
     
-    async def get_current_active_user(current_user: UserSchema = Depends(get_current_user)):
+    async def get_current_active_user(current_user: UserSchema.User = Depends(get_current_user)):
         # una funcion que obtiene un usuario actual activo a partir del token
         if current_user.status == 10:
             raise HTTPException(status_code=400, detail="Inactive user")
